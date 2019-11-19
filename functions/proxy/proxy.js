7393c73837c75853082dcbca31d4357ddf4fd2f1
samo4 <samo.4tuna@gmail.com>
2019-11-19 18:48:19 +0100

    another try

--- functions/proxy/proxy.js
+++ functions/proxy/proxy.js
@@ -1,7 +1,7 @@
 "use strict";
 
-import fetch from "node-fetch";
-// const fetch = require("node-fetch");
+//import fetch from "node-fetch";
+const fetch = require("node-fetch");
 
 exports.handler = async ( event , context ) => { 
   return { statusCode : 200 , body : " We are now split testing! " + process.version }
